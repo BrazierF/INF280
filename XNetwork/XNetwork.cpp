72e372005c1c107f52aaaf07e0a58fcba289b234
Brazier <franck.brazier@telecom-paristech.fr>
2015-05-18 11:41:10 +0200

    Create XNetwork.cpp

---
+++ XNetwork/XNetwork.cpp
@@ -0,0 +1,48 @@
+
+#include<iostream>
+#include <algorithm>
+#include<vector>
+using namespace std;
+
+int main(){
+    int nb,nbinput,id,swag,monami,src,dst;
+    cin>>nbinput;
+    for(int i=0;i<nbinput;i++){
+        cin>>nb;
+        vector<pair<int,vector<int> > > monreseau;
+        vector<vector< int> > memoire (nb,vector<int>());
+        bool trouve=false;
+        for(int bf=0;bf<nb;bf++){
+            cin>>id>>swag;
+            for(int amis=0;amis<swag;amis++){
+                cin>>monami;
+                memoire.at(id).push_back(monami);
+            }
+        }
+        cin>>src>>dst;
+        monreseau.push_back(make_pair(0,vector<int>(1,src)));
+        int index=0;
+        if(find(monreseau.at(index).second.begin(),monreseau.at(index).second.end(),dst)!=monreseau.at(index).second.end())
+            trouve=true;
+        while(!trouve){
+            //cout<<"Iteration n:"<<index<<"  ";
+            monreseau.push_back(make_pair(index+1,vector<int>()));
+            for(int toto:monreseau.at(index).second){
+                monreseau.at(index+1).second.insert(monreseau.at(index+1).second.end(),memoire.at(toto).begin(),memoire.at(toto).end());
+                //cout<<"Salut ";
+                /*for(int prout:memoire.at(toto)){
+                   cout<<prout<<"|";
+                 }
+                cout<<endl;
+                cout<<toto<<" | ";*/
+            }
+           // cout<<endl;
+            index++;
+            if(find(monreseau.at(index).second.begin(),monreseau.at(index).second.end(),dst)!=monreseau.at(index).second.end())
+                trouve=true;
+        }
+        cout<<src<<" "<<dst<<" "<< index-1<<endl;
+        if(i<nbinput-1)
+            cout<<"\n";
+    }
+}
